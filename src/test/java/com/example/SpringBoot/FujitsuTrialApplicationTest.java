2cf40f2b68ecfebf7c032c894052e700f7299c2b
Erik Presnov <erik.presnov160@gmail.com>
2023-03-20 19:45:56 +0200

    Fixed API test

--- src/test/java/com/example/SpringBoot/FujitsuTrialApplicationTest.java
+++ src/test/java/com/example/SpringBoot/FujitsuTrialApplicationTest.java
@@ -19,10 +19,30 @@ class FujitsuTrialApplicationTest {
     @Autowired
     private MockMvc mvc;
 
+
+    //tallinn	26038	6.5 2.3	light rain	1679332081
     @Test
     public void calculateFeeOnGivenTimestamp() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/%s/%s/%d".formatted("tartu", "car", 1679320713));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/%s/%s/%d".formatted("tallinn", "car", 1679332081));
         MvcResult result = mvc.perform(requestBuilder).andReturn();
-        assertEquals(0,Integer.parseInt(result.getResponse().getContentAsString()));
+        // Tallinn / Car -> BaseFee = 4.0
+        // no extra fees for airTemp, windSpeed or phenomenon since vehicleType = car
+        assertEquals(4.0,Double.parseDouble(result.getResponse().getContentAsString()));
+
+        requestBuilder = MockMvcRequestBuilders.get("/%s/%s/%d".formatted("tallinn", "scooter", 1679332081));
+        result = mvc.perform(requestBuilder).andReturn();
+        // Tallinn / scooter -> BaseFee = 3.5
+        // rain -> phenomenonFee = 0.5
+        // temp = 6.5 so no extra fee
+        // wind = 2.3 so no extra fee
+        assertEquals(4.0,Double.parseDouble(result.getResponse().getContentAsString()));
+
+        requestBuilder = MockMvcRequestBuilders.get("/%s/%s/%d".formatted("tallinn", "bike", 1679332081));
+        result = mvc.perform(requestBuilder).andReturn();
+        // Tallinn / bike -> BaseFee = 3.0
+        // rain -> phenomenonFee = 0.5
+        // temp = 6.5 so no extra fee
+        // wind = 2.3 so no extra fee
+        assertEquals(3.5,Double.parseDouble(result.getResponse().getContentAsString()));
     }
 }
